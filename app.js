ead9474c969c89eadae77105523c336b4f0f9a43
aamocca <aaroncafeina@hotmail.com>
2022-01-31 14:46:09 -0600

    Story Users #1 y #2

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+  console.log("El servidor esta corriendo");
+});
+
+app.get("/", (req, res) => {
+  res.render("./pages/index.ejs");
+});
+
+app.get("/products/:id", (req, res) => {
+  res.render("./pages/producto.ejs");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("./pages/checkout.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("./pages/register.ejs");
+});
+app.get("/login", (req, res) => {
+  res.render("./pages/login.ejs");
+});
