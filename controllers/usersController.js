45963194225427790c6ed9629520716870b554b1
aamocca <aaroncafeina@hotmail.com>
2022-02-09 16:05:50 -0600

    Registro completo

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,23 +2,23 @@ const req = require("express/lib/request");
 const functions = require("../models/userModel.js");
 
 let usersMethod = {
-    registro: (req, res) => {
-        let arregloUsuario = functions.leerJson();
-        let { email, pass, ValidPass } = req.body;
-        if (pass == ValidPass) {
-            if (arregloUsuario.find((usuario) => usuario.email == email)) {
-                res.send("Error");
-            } else {
-                arregloUsuario.push({
-                    email: email,
-                    password: pass,
-                });
-            }
-        }
-        res.redirect("/login");
-        // functions.escribirJson(arregloUsuario);
-        console.log(arregloUsuario);
-    },
+  registro: (req, res) => {
+    let arregloUsuario = functions.leerJson();
+    let { email, pass, ValidPass } = req.body;
+    if (pass == ValidPass) {
+      if (arregloUsuario.find((usuario) => usuario.email == email)) {
+        res.send("Error");
+      } else {
+        arregloUsuario.push({
+          email: email,
+          password: pass,
+        });
+      }
+    }
+
+    functions.escribirJson(arregloUsuario);
+    res.redirect("/login");
+  },
 };
 
 module.exports = usersMethod;
